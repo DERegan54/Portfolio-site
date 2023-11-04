edc81c0dd0c259caa03ae1b467b439467b92a078
Danielle Regan Arnett <regandanielle54@gmail.com>
2023-11-04 09:45:30 -0600

    Update resume img

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './index.scss';
-import ResumeImg from "../../assets/images/10-23_Resume.png"
+import ResumeImg from "../../assets/images/Nov23-resume copy.pdf"
 import Sidebar from '../Sidebar';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
