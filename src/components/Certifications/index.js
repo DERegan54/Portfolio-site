7f0d8dbe5107c8453b49b9ff9e03c278fd88159d
Danielle Regan Arnett <regandanielle54@gmail.com>
2023-11-27 15:01:51 -0700

    Added Agile badge

--- src/components/Certifications/index.js
+++ src/components/Certifications/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './index.scss';
 import SpringboardImg from "../../assets/images/Springboard-cert.png"
+import AgileScrumFundImg from "../../assets/images/Agile-scrum-fund-badge.png";
 import Sidebar from '../Sidebar';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
@@ -17,12 +18,13 @@ const Certifications = () => {
                     <h1 className='cert-h1'>
                         <AnimatedLetters 
                             letterClass={letterClass}
-                            strArray={["M","y", " ", "C", "e", "r", "t", "i", "f", "i", "c", "a", "t", "i", "o", "n", "s", " ", ":"]}
+                            strArray={["M","y", " ", "C", "e", "r", "t", "i", "f", "i", "c", "a", "t", "e", "s", " ", ":"]}
                             idx={15}
                         />
                     </h1>   
                 </div>
                 <img src={SpringboardImg} className='SpringboardImage' />
+                <img src={AgileScrumFundImg} className='AgileScrumFundImg' />
             </div>
             <Loader type='pacman' />
         </>    
